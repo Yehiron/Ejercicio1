ae4bfc84f2de20a4791dd96750ce87010303a0d8
Yehiron <yeiron12@gmail.com>
2026-02-26 10:43:24 -0500

    Modificando main desde la rama feature-ff

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Excersice 1!");
+        System.out.println("Hello, from branch feature-ff");
     }
 }
